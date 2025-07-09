1977e3052e77eef266b0479701f8018d371714f3
Shahmir Khan <shahmirkhan740@gmail.com>
2025-07-10 00:49:44 +0500

    change class name
    change class name swiper container

--- src/app/components/sections/SparkTier.tsx
+++ src/app/components/sections/SparkTier.tsx
@@ -74,7 +74,7 @@ const SparkTier = ({ isActive = false }: SlideProps) => {
             pagination={{ clickable: true }}
             spaceBetween={30}
             slidesPerView={1}
-            className="pb-[calc(var(--spacing)*6)] !important"
+            className="swiper-container pb-6"
           >
             {/* Slides */}
             <SwiperSlide className="space-y-6">
